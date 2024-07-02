d6445f1f2c2ffa7040d56a5a8e13615505085aac
Jeff Killinger <jeffkillinger@gmail.com>
2024-07-02 01:48:30 -0400

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default function About() {
+    return (
+      <main>
+        <h2>This page is all about ol' Jeffrey</h2>
+      </main>
+    );
+  }
+  
---
+++ app/blogs/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Blog() {
+    return (
+      <main>
+        <h2>Wait this is the blog page now</h2>
+      </main>
+    );
+  }
+  
